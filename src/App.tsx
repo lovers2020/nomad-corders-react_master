764d3ed9d4bf5b69582cee1908bd3f9f044ff4c8
lovers2020 <dkrkskswl@naver.com>
2023-10-25 23:50:11 +0900

    Update file

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
